affcab9e53825704163c9ea367b4b02fde7679c2
Ida <idla0004@stud.kea.dk>
2026-01-29 23:19:23 +0100

    tilføjet animationer

--- computergaet_oevelse/index.js
+++ computergaet_oevelse/index.js
@@ -3,52 +3,61 @@ const tooHigh = document.querySelector(".toohigh");
 const tooLow = document.querySelector(".toolow");
 const youWon = document.querySelector(".winner");
 const startGame = document.querySelector(".startgame");
+const higherImg = document.querySelector(".higher-img");
+const lowerImg = document.querySelector(".lower-img");
+const winnerImg = document.querySelector(".winner-img");
+const hidden = document.querySelector(".hidden");
 let min = 0;
 let max = 100;
 let number;
 
-// skal starte computerens gæt
+// Start computerens gæt
 startGame.addEventListener("click", function () {
-  number = Math.ceil(Math.random(min + max) * 100);
+  // Skjul alle billeder ved start
+  higherImg.classList.add("hidden");
+  lowerImg.classList.add("hidden");
+  winnerImg.classList.add("hidden");
+
+  // starter computerens gæt
+  number = Math.floor((min + max) / 2); // regner ud at midten af min og max er 100 og bliver 50
   document.querySelector(".computeranswer").innerHTML =
     "Jeg gætter på " + number;
   console.log("Nyt tal:", number);
 
   tooHigh.addEventListener("click", (e) => {
-    number = Math.floor(number - 2);
+    max = number - 1; // næste gæt efter 50 er derfor max = 49
+    number = Math.floor((min + max) / 2); // dividerer 49 med 2 og får 24
     console.log(" tal:", number);
     document.querySelector(".computeranswer").innerHTML =
       "Er det " + number + "?";
+
+    // Vis billedet for "gæt lavere"
+    lowerImg.classList.remove("hidden");
+    higherImg.classList.add("hidden");
+    winnerImg.classList.add("hidden");
     e.preventDefault();
   });
 
   tooLow.addEventListener("click", (e) => {
-    number = Math.ceil(number + 2);
+    min = number + 1; // næste gæt efter 50 er derfor max = 51
+    number = Math.floor((min + max) / 2); // det sætter tal gættet ned ved at min er 51, max er 100 og så dividerer med 2 hvilket bliver = 75
     console.log(" tal:", number);
     document.querySelector(".computeranswer").innerHTML =
       "Er det " + number + "?";
+    // Vis billedet for "gæt højere"
+    higherImg.classList.remove("hidden");
+    lowerImg.classList.add("hidden");
+    winnerImg.classList.add("hidden");
     e.preventDefault();
   });
 
   youWon.addEventListener("click", (e) => {
     document.querySelector(".computeranswer").innerHTML = "Du vandt!";
+    // Vis billedet for "vinder"
+    winnerImg.classList.remove("hidden");
+    higherImg.classList.add("hidden");
+    lowerImg.classList.add("hidden");
+
     e.preventDefault();
   });
 });
-
-// computer vandt, men skal gætte igen
-youWon.addEventListener("click", youWon);
-
-// guessNumber();
-
-// function guessNumber() {
-//   const messageElement = document.getElementById("message");
-//   if (number === realNumber) {
-//     messageElement.textContent = "Tillykke, du gættede rigtigt!";
-//   } else if (number > realNumber)
-//     messageElement.textContent = "Haha det er for lavt";
-//   else if (number < realNumber)
-//     messageElement.textContent = "Haha det er for højt";
-// }
-// console.log("Nyt tal:", number);
-// man kan erklære to variabler for hvad den mindst må være og hvad den højest må være. min(0) max(100)
