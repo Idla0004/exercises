b11d0d8be3aae31a2bb33da5d7dc05a5348ac6cd
Ida <idla0004@stud.kea.dk>
2026-02-06 13:43:42 +0100

    Capitalization

---
+++ String_oevelser/capitalization3.js
@@ -0,0 +1,27 @@
+const string1 = "Peter";
+const string2 = "peTer";
+const mixedString = string1
+  .substring(0, 5)
+  .concat(", nej jeg hedder ", string2);
+
+console.log(string1.toUpperCase());
+
+console.log(string2.toLowerCase());
+
+console.log(string2.concat(", ", string1));
+
+console.log(mixedString);
+
+const string3 = "SUSANNE";
+const string4 = "suSaNnE";
+const mixedString2 = string3
+  .substring(0, 8)
+  .concat(", nej jeg hedder ", string4);
+
+console.log(string3.toUpperCase());
+
+console.log(string4.toLowerCase());
+
+console.log(string3.concat(", ", string4));
+
+console.log(mixedString2);
