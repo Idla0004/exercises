db4e862f32a8ef8ef0383475653e297b87c83f8b
Ida <idla0004@stud.kea.dk>
2026-02-06 13:31:25 +0100

    Name parts

---
+++ name_parts_oevelse/nameparts.js
@@ -0,0 +1,44 @@
+const fullName = "Peter Heronimous Lind";
+const firstName = fullName.substring(0, 5);
+const middleName = "Heronimous";
+const lastName = "Lind";
+const indexOfMiddle = fullName.indexOf(middleName);
+const indexOfLast = fullName.indexOf(lastName);
+
+// Find fulde navn
+console.log("Full name is", fullName);
+
+// find fornavn
+console.log("First name is", firstName);
+
+// find mellemnavn
+console.log(`Middle name is ${middleName}`);
+
+// find efternavn
+console.log(`Last name is ${lastName}`);
+
+// test med eget navn
+const myName = "Ida Hernvig Larsen";
+const myFirstName = myName.substring(0, 3);
+const myMiddlename = myName.substring(4, 11);
+const myLastName = "Larsen";
+const indexOfMyLast = myName.indexOf(myLastName);
+
+// Find fulde navn
+console.log("My full name is", myName);
+
+// find fornavn
+console.log("My first name is", myFirstName);
+
+// find mellemnavn
+console.log("My middle name is", myMiddlename);
+
+// find efternavn
+console.log(`My last name is ${myLastName}`);
+
+const name = "Ida Hernvig Larsen";
+console.log(name.indexOf(" "));
+console.log(name.lastIndexOf(" "));
+
+// Jeg forstår ikke om navnet skal skrives ud i console.log eller om index tallet skal?
+// har derfor lavet flere muligheder.
