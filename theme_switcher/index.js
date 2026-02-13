4a7f024e816aec9b9ef65faab0610114b32bb761
Ida <idla0004@stud.kea.dk>
2026-02-13 14:23:31 +0100

    opdateret theme switcher med local storage

--- theme_switcher/index.js
+++ theme_switcher/index.js
@@ -1,4 +1,15 @@
+console.log("Hvilket theme er gemt", localStorage.getItem("savedtheme"));
+const localStorageTheme = localStorage.getItem("savedtheme");
+
+if (localStorageTheme) {
+  document.querySelector("body").dataset.theme = localStorageTheme; // gemmer tema valgt i localStorage, ved refresh er temaet stadig det valgte, indtil jeg slette i local storage.
+} else {
+  document.querySelector("body").dataset.theme = "dark"; // gør dark til default tema
+  document.querySelector(".theme").value = "dark"; // refresher drop-down til at vise det rigtige tema (dark)
+}
+
 document.querySelector(".theme").addEventListener("change", selectTheme);
 function selectTheme(evt) {
+  localStorage.setItem("savedtheme", evt.target.value); // henter localstorage i console.log > storage > local storage
   document.querySelector("body").dataset.theme = evt.target.value;
 }
