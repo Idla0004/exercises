22fb65e964f7053f863ae6c5f1981967de5bd581
Ida <idla0004@stud.kea.dk>
2026-02-16 18:06:15 +0100

    Fået tekst til at flytte sig til done liste

--- todo_opgave/index.js
+++ todo_opgave/index.js
@@ -1,13 +1,17 @@
 const todoText = document.querySelector("#todo_input");
-const todoBtn = document.querySelector(".todobtn");
+const todoBtn = document.querySelector(".writetodobtn");
 const todoContainer = document.querySelector(".todo_container");
 const unchecked = document.querySelector(".unchecked");
 const todoArr = [];
+const notdoneBtn = document.querySelector("#filtertodo");
+const doneBtn = document.querySelector("#filterdone");
+const doneContainer = document.querySelector(".done_container");
 
 todoBtn.addEventListener("click", submitToDo);
+notdoneBtn.addEventListener("click", () => showList("todo"));
+doneBtn.addEventListener("click", () => showList("done"));
 
 function submitToDo() {
-  //   todoArr.length = 0; // starter med at "cleare" det udskrevede/synlige array, så skrevne ting ikke gentager sig, når man klikker på btn.
   const todoObject = {
     text: todoText.value,
     done: false,
@@ -16,17 +20,19 @@ function submitToDo() {
     id: self.crypto.randomUUID(),
   };
   todoArr.push(todoObject);
+  todoText.value = "";
   console.log("todoArr", todoArr);
   filterAndSortTaskArr();
 }
 
 function filterAndSortTaskArr() {
   showTaskArr();
-  //   todoArr.push(todoText);
 }
 
 function showTaskArr() {
   todoContainer.innerHTML = "";
+  doneContainer.innerHTML = "";
+
   todoArr.forEach((elm) => {
     const li = document.createElement("li");
     li.innerHTML = `<p class="checkbox">${elm.unchecked ? "o" : "✓"}
@@ -34,23 +40,26 @@ function showTaskArr() {
     <p class="textelement">${elm.text}</p>
      <p class="star">${elm.star ? "☆" : "★"}</p> `;
 
-    todoContainer.appendChild(li);
-    todoText.value = "";
+    if (elm.unchecked) {
+      todoContainer.appendChild(li);
+    } else {
+      doneContainer.appendChild(li);
+    } // flytter to do tekst til done containeren
 
     li.addEventListener("click", (evt) => {
       if (evt.target.classList.contains("checkbox")) {
         console.log("vis ikon");
-        elm.unchecked = !elm.unchecked; // (! gør den til det modsatte)
-        filterAndSortTaskArr();
-        console.log("stjerne klikket", `${elm.id}`); // tjekker id i console log
+        elm.unchecked = !elm.unchecked; // (! skifter til checked status)
       }
+      showTaskArr(); // opdaterer visningen
+      console.log("check boxed is check of", `${elm.id}`); // tjekker id i console log
     });
 
     li.addEventListener("click", (event) => {
       if (event.target.classList.contains("star")) {
         console.log("vis ikon");
-        elm.star = !elm.star; // (! gør den til det modsatte)
-        filterAndSortTaskArr();
+        elm.star = !elm.star; // skifter favorit status
+        showTaskArr(); // opdaterer visningen
       }
     });
   });
