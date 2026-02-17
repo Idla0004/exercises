3296351950795843df93849c3d7c85ba60202dc9
Ida <idla0004@stud.kea.dk>
2026-02-17 10:33:56 +0100

    To do opdateret med flere funktioner

--- todo_opgave/index.js
+++ todo_opgave/index.js
@@ -3,13 +3,30 @@ const todoBtn = document.querySelector(".writetodobtn");
 const todoContainer = document.querySelector(".todo_container");
 const unchecked = document.querySelector(".unchecked");
 const todoArr = [];
-const notdoneBtn = document.querySelector("#filtertodo");
-const doneBtn = document.querySelector("#filterdone");
+const notdoneBtn = document.querySelector("#btntodo");
+const doneBtn = document.querySelector("#btndone");
 const doneContainer = document.querySelector(".done_container");
+const todoList = document.querySelector("#todolist");
+const doneList = document.querySelector("#donelist");
 
 todoBtn.addEventListener("click", submitToDo);
-notdoneBtn.addEventListener("click", () => showList("todo"));
-doneBtn.addEventListener("click", () => showList("done"));
+notdoneBtn.addEventListener("click", showNotDonelist);
+doneBtn.addEventListener("click", showDonelist);
+
+function showDonelist() {
+  todoList.style.zIndex = "0";
+  doneList.style.zIndex = "1";
+  doneBtn.style.zIndex = "1";
+  doneBtn.style.backgroundColor = "#f0ede4";
+  notdoneBtn.style.backgroundColor = "#79744e";
+  console.log("Knap er klikket", notdoneBtn);
+}
+
+function showNotDonelist() {
+  todoList.style.zIndex = "1";
+  doneList.style.zIndex = "0";
+  doneBtn.style.zIndex = "0";
+}
 
 function submitToDo() {
   const todoObject = {
@@ -17,6 +34,7 @@ function submitToDo() {
     done: false,
     unchecked: true,
     star: true,
+    delete: true,
     id: self.crypto.randomUUID(),
   };
   todoArr.push(todoObject);
@@ -43,8 +61,13 @@ function showTaskArr() {
     if (elm.unchecked) {
       todoContainer.appendChild(li);
     } else {
-      doneContainer.appendChild(li);
-    } // flytter to do tekst til done containeren
+      doneContainer.appendChild(li); // flytter to do tekst til done containeren
+      li.innerHTML = `<p class="checkbox">${elm.unchecked ? "o" : "✓"}
+    </p>
+    <p class="textelement">${elm.text}</p>
+     <p class="star">${elm.star ? "☆" : "★"}</p>
+     <img src="img/delete.webp" class="deleteimg" ${elm.delete}></img>`;
+    }
 
     li.addEventListener("click", (evt) => {
       if (evt.target.classList.contains("checkbox")) {
@@ -53,19 +76,22 @@ function showTaskArr() {
       }
       showTaskArr(); // opdaterer visningen
       console.log("check boxed is check of", `${elm.id}`); // tjekker id i console log
-    });
-
-    li.addEventListener("click", (event) => {
-      if (event.target.classList.contains("star")) {
+      if (evt.target.classList.contains("star")) {
         console.log("vis ikon");
         elm.star = !elm.star; // skifter favorit status
         showTaskArr(); // opdaterer visningen
       }
+      if (evt.target.classList.contains("deleteimg")) {
+        // hvis event indeholder delete billedet
+        const index = todoArr.findIndex(
+          (targetArray) => targetArray.id === elm.id, // index finder indexet på det specifikke liste element.
+        );
+        todoArr.splice(index, 1); // hvis trykket på, så bliver elementet/to do teksten slettet.
+        filterAndSortTaskArr();
+      }
     });
   });
 }
-
-// Filtrer med knapper
 
 //gamle liste
 
